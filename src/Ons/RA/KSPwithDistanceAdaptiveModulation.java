8d9c3a5ffac7c264b48077d9ecb3709bc06ba5f4
Mehdi-Habibi <me.habibi06@gmail.com>
2016-12-15 05:07:24 +0330

    last changes

--- src/Ons/RA/KSPwithDistanceAdaptiveModulation.java
+++ src/Ons/RA/KSPwithDistanceAdaptiveModulation.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
  * Aug 2012.
  * @author onsteam
  */
-public class KSPBestModulation implements RA {
+public class KSPwithDistanceAdaptiveModulation implements RA {
 
     private ControlPlaneForRA cp;
     private WeightedGraph graph;
@@ -34,102 +34,70 @@ public class KSPBestModulation implements RA {
 
     @Override
     public void flowArrival(Flow flow) {
-        int maxModulation = 0;
-        double slotNum = 0d;
-        double rate = 0d;
-        long counting = 0;
         int[] nodes;
         int[] links;
         long id;
         LightPath[] lps = new LightPath[1];
 
         // Set k=4
-        int ksp = 1;
+        int ksp = 3;
 
         // k-Shortest Paths routing
+
         ArrayList<Integer>[] kpaths = YenKSP.kShortestPaths(graph, flow.getSource(), flow.getDestination(), ksp);
 
         for (int k = 0; k < kpaths.length; k++) {
+
             nodes = route(kpaths,k);
             // If no possible path found, block the call
             if (nodes.length == 0 || nodes == null) {
                 cp.blockFlow(flow.getID());
                 return;
             }
+
             // Create the links vector
             links = new int[nodes.length - 1];
             for (int j = 0; j < nodes.length - 1; j++) {
                 links[j] = cp.getPT().getLink(nodes[j], nodes[j + 1]).getID();
             }
-            rate = (double)flow.getRate() * Math.pow(10,6);
-            slotNum = (rate / SNR.slotSize);
-            //System.out.println("slotNum: " + Double.toString(slotNum));
-            for(int vvv = (Modulation.N_MOD - 1); vvv > -1; vvv--){
-                if(vvv == (Modulation.N_MOD - 1)){
-                    if(slotNum >= (vvv)){
-                        maxModulation = vvv;
-                        break;
-                    }
-                }else{
-                    if((slotNum >= (vvv)) && (slotNum < (vvv + 1))){
-                        maxModulation = vvv;
-                        break;
-                    }
-                }
+
+            // Get the size of the route in km
+            double sizeRoute = 0;
+            for (int i = 0; i < links.length; i++) {
+                sizeRoute += ((EONLink) cp.getPT().getLink(links[i])).getWeight();
             }
-            //System.out.println("maxModulation: " + Integer.toString(maxModulation));
-            for(int mod = maxModulation; mod > -1; mod--){
-                this.modulation = mod;
-                // Calculates the required slots
-                int requiredSlots = Modulation.convertRateToSlot(flow.getRate(), EONPhysicalTopology.getSlotSize(), modulation);
-                // First-Fit spectrum assignment in BPSK Ons.Modulation
-                int[] firstSlot;
+            // Adaptative modulation:
+            this.modulation = Modulation.getBestModulation(sizeRoute);
+
+            // Calculates the required slots
+            int requiredSlots = Modulation.convertRateToSlot(flow.getRate(), EONPhysicalTopology.getSlotSize(), modulation);
+
+            // First-Fit spectrum assignment in BPSK Ons.Modulation
+            int[] firstSlot;
+            for (int i = 0; i < 1; i++) {
                 // Try the slots available in each link
-                firstSlot = ((EONLink) cp.getPT().getLink(links[0])).getSlotsAvailableToArray(requiredSlots);
+                firstSlot = ((EONLink) cp.getPT().getLink(links[i])).getSlotsAvailableToArray(requiredSlots);
                 for (int j = 0; j < firstSlot.length; j++) {
                     // Now you create the lightpath to use the createLightpath VT
-                    EONLightPath lp = cp.createCandidateEONLightPath(flow.getSource(), flow.getDestination(), links, firstSlot[j],
-                            (firstSlot[j] + requiredSlots - 1), modulation);
+                    EONLightPath lp = cp.createCandidateEONLightPath(flow.getSource(), flow.getDestination(), links,
+                            firstSlot[j], (firstSlot[j] + requiredSlots - 1), modulation);
                     // Now you try to establish the new lightpath, accept the call
                     if ((id = cp.getVT().createLightpath(lp)) >= 0) {
                         // Single-hop routing (end-to-end lightpath)
                         lps[0] = cp.getVT().getLightpath(id);
-
-                        EONLink[] usedLinks = new EONLink[links.length];
-                        for (int xx = 0; xx < links.length; xx++){
-                            usedLinks[xx] = (EONLink) cp.getPT().getLink(links[xx]);
-                        }
-
-                        SNR snr = new SNR(lp,usedLinks );
-                        double lightpathSNR = snr.getLightPathSNR();
-                        //System.out.println("lightpathSNR: "+Double.toString(lightpathSNR));
-
-                        if (cp.MyacceptFlow(flow.getID(), lps)) {
-                            counting++;
-                            if(Modulation.getSNR(modulation) < lightpathSNR) {
-                                cp.acceptFlow(flow.getID(), lps);
-                                return;
-                            }
-                            else{
-                                cp.getVT().deallocatedLightpath(id);
-                            }
+                        if (cp.acceptFlow(flow.getID(), lps)) {
+                            return;
                         } else {
                             // Something wrong
                             // Dealocates the lightpath in VT and try again
                             cp.getVT().deallocatedLightpath(id);
                         }
-
                     }
                 }
             }
         }
         // Block the call
-        if(counting > 0){
-            cp.SNRblockFlow(flow.getID());
-        }
-        else{
-            cp.blockFlow(flow.getID());
-        }
+        cp.blockFlow(flow.getID());
     }
 
     @Override
